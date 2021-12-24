7e49fac4e87b1560c4fb164af3da94f9702698d8
Bereket Semagn <semagnbereket@gmail.com>
2021-12-24 17:42:07 -0500

    feat: finish layout

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex flex-col min-h-screen bg-dark-tertiary">
+					<Component {...pageProps} />
+				</div>
+  )
 }
 
 export default MyApp
