5049a80cedd18823ed962d300ae5f956a2ab459b
Bereket Semagn <semagnbereket@gmail.com>
2021-12-26 07:50:23 -0500

    fix: API_URL variable

--- packages/client/lib/constants.ts
+++ packages/client/lib/constants.ts
@@ -1,1 +1,1 @@
-export const API_URL = `${process.env.API_URL}/v1` ?? "http://localhost:8080/v1";
+export const API_URL = process.env.API_URL ? `${process.env.API_URL}/v1` : "http://localhost:8080/v1";
