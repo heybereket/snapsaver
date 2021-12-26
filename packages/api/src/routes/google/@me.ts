0c95d0df242c0d824c17fdec0363f43672ae0957
Bereket Semagn <semagnbereket@gmail.com>
2021-12-26 08:32:48 -0500

    fix: remove logout on /v1/google/@me

--- packages/api/src/routes/google/@me.ts
+++ packages/api/src/routes/google/@me.ts
@@ -2,7 +2,6 @@ import { FastifyPluginCallback } from "fastify";
 
 const googleMe: FastifyPluginCallback = async (fastify) => {
   fastify.get("/@me", async (req, res) => {
-    req.logout();
     res.send({
       success: true,
       data: req.user,
