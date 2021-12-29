77b5a10b750dd838f98827021f3610627aeb6777
Bereket Semagn <semagnbereket@gmail.com>
2021-12-29 16:36:35 -0500

    fix useUser hook url

--- packages/client/hooks/useUser.ts
+++ packages/client/hooks/useUser.ts
@@ -1,7 +1,7 @@
 import useSWR from 'swr';
 
 export const useUser = () => {
-	const {data, error} = useSWR(`google/@me`);
+	const {data, error} = useSWR(`auth/google/@me`);
 	const user = data?.data;
 
 	return {
