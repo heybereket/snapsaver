24a333ed849114ba842c26499abab66f1389d4fd
Bereket Semagn <semagnbereket@gmail.com>
2022-01-02 01:38:49 -0500

    fix: remove beta users

--- packages/api/src/lib/constants.ts
+++ packages/api/src/lib/constants.ts
@@ -11,8 +11,8 @@ export const COOKIE_NAME = process.env.COOKIE_NAME ?? "snapsaver-token";
 export const CLIENT_URL = process.env.CLIENT_URL ?? "http://localhost:3000";
 export const S3_BUCKET = process.env.AWS_BUCKET_NAME as string;
 
-// Other
-export const BetaAllowedUsers = {
-  ENABLED: true,
-  EMAILS: process.env.BETA_EMAILS?.split(",") ?? [],
-};
+// // Other
+// export const BetaAllowedUsers = {
+//   ENABLED: true,
+//   EMAILS: process.env.BETA_EMAILS?.split(",") ?? [],
+// };
