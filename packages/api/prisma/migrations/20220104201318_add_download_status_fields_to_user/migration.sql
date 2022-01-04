6fd87f32b811ae0f7db9ae191a272045f8fcdf12
Addis Semagn <asemagn@gmail.com>
2022-01-04 20:14:26 +0000

    chore: migration addDownloadStatusFieldsToUser

---
+++ packages/api/prisma/migrations/20220104201318_add_download_status_fields_to_user/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "memoriesFileId" TEXT,
+ADD COLUMN     "memoriesFiled" INTEGER,
+ADD COLUMN     "memoriesPending" INTEGER,
+ADD COLUMN     "memoriesSuccess" INTEGER;
