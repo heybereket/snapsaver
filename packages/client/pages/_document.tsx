a0fba4bdfa79196731e5c5ae4e31c171ab927d72
Bereket Semagn <semagnbereket@gmail.com>
2022-01-05 21:13:17 -0500

    bruh

--- packages/client/pages/_document.tsx
+++ packages/client/pages/_document.tsx
@@ -6,7 +6,18 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <link rel="shortcut icon" href="/assets/snapsaver-logo.png" />
+          <meta
+            name="og:description"
+            content="Backup your Snapchat Memories to Google Drive"
+          />
+          <meta
+            name="twitter:description"
+            content="Backup your Snapchat Memories to Google Drive"
+          />
+          <meta name="twitter:image" content="/assets/banner.png" />
+          <meta name="theme-color" content="#FED8B1" />
+          <link rel="shortcut icon" href="/assets/snapsaver-logo.png" />
+          <meta name="og:image" content="/assets/banner.png" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
             rel="stylesheet"
