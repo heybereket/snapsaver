cb470d017b317fe68a309105dbb10064e3fa1f54
Addis Semagn <asemagn@gmail.com>
2022-01-06 16:42:40 +0000

    feat: access code for beta

--- packages/client/components/Header.tsx
+++ packages/client/components/Header.tsx
@@ -3,20 +3,23 @@ import { Google } from "./icons/Google";
 import Image from "next/image";
 import axios from "axios";
 import Link from "next/link";
+import { useState } from "react";
 
 export const Header = (props: { data: any }) => {
-  const notSignedUpBetaUser =
-    props.data.message && props.data.message.includes("Sign up");
-  const notApprovedBetaUser =
-    props.data.message && props.data.message.includes("beta");
-  console.log(props.data, notSignedUpBetaUser, notApprovedBetaUser);
+  const joinedBeta = props.data?.user?.betaUser;
+  const [accessCode, setAccessCode] = useState("");
+  const [accessCodeError, setAccessCodeError] = useState("");
 
   const joinBetaHandler = async () => {
-    await axios.get(`${API_URL}/@me/beta`, {
-      withCredentials: true,
-    });
+    if (accessCode === process.env.NEXT_PUBLIC_BETA_ACCESS_CODE) {
+      await axios.get(`${API_URL}/@me/beta`, {
+        withCredentials: true,
+      });
 
-    window.location.reload();
+      window.location.reload();
+    } else {
+      setAccessCodeError("incorrect passphrase. try again?");
+    }
   };
 
   return (
@@ -81,23 +84,31 @@ export const Header = (props: { data: any }) => {
         </div>
       </div>
 
-      {props.data.user && (notApprovedBetaUser || notSignedUpBetaUser) && (
+      {props.data.user && !joinedBeta && (
         <>
-          <div className="flex items-center justify-center">
-            <span>
-              {notSignedUpBetaUser
-                ? `You aren't a beta user...yet. Sign up for the waitlist.`
-                : `You're not approved for beta yet! Soon...`}
-            </span>
+          <div className="flex flex-col items-center justify-center">
+            <span className="mb-3">Enter the passphrase to join the beta.</span>
+            <input
+              className={`w-[150px] px-5 py-3 text-secondary bg-navbar rounded-lg display-none md:block"
+                }`}
+              onChange={(e) => setAccessCode(e.target.value)}
+            />
+            {accessCodeError ? (
+              <div className="flex items-center justify-center mt-5">
+                <span className="w-[410px] text-red-500 text-center">
+                  {accessCodeError}
+                </span>
+              </div>
+            ) : (
+              <div></div>
+            )}
           </div>
           <div className="flex items-center justify-center mt-5">
             <button
-              onClick={notSignedUpBetaUser ? joinBetaHandler : undefined}
-              className={`group bg-navbar px-5 py-3 flex items-center text-sm w-auto rounded-lg cursor-pointer transition ease-out hover:bg-primary hover:text-black text-center text-secondary ${
-                !notSignedUpBetaUser && "cursor-not-allowed opacity-50"
-              }`}
+              onClick={joinBetaHandler}
+              className={`group bg-navbar px-5 py-3 flex items-center text-sm w-auto rounded-lg cursor-pointer transition ease-out hover:bg-primary hover:text-black text-center text-secondary`}
             >
-              {notSignedUpBetaUser ? "Join waitlist" : "Joined beta"}
+              submit
             </button>
           </div>
         </>
